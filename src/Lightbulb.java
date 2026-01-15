fc70cae9ce58cb418131feed39212d5e2788b7aa
Morgan Chan <99379848+GLMortyc@users.noreply.github.com>
2026-01-15 13:24:28 -0700

    Update Lightbulb.java

--- src/Lightbulb.java
+++ src/Lightbulb.java
@@ -1,4 +1,28 @@
 public class Lightbulb {
+    private String name;
+    private boolean isOn;
+    private int powerUsage;
 
+    public Lightbulb(String n, boolean O, int U) {
+        name = n;
+        isOn = O;
+        powerUsage = U;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean getIsOn() {
+        return isOn;
+    }
+    public int getPowerUsage(){
+        return powerUsage;
+    }
+    public void turnOn(){
+        isOn = true;
+    }
+    public void turnOff(){
+        isOn = false;
+    }
 
 }
