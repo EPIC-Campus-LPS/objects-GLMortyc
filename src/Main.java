34807626e05c31c45c5e7501b35d9f77dbaa6f17
Morgan Chan <99379848+GLMortyc@users.noreply.github.com>
2026-01-15 13:24:01 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        // This is the main
+    static void main(String[] args) {
+        Lightbulb light1 = new Lightbulb("Kitchen", false, 60);
+        Lightbulb light2 = new Lightbulb("Dining Room", false, 80);
+        Lightbulb light3 = new Lightbulb("Living Room", false, 40);
+
+        System.out.println(light1.getName()); // Kitchen
+
+        light1.turnOn();
+        System.out.println(light1.getIsOn()); // true
+        light1.turnOff();
+        System.out.println(light1.getIsOn()); // false
+
+        System.out.println(light2.getPowerUsage()); // 80
+
+        SmartHome home = new SmartHome(3);
+
+        home.add(light1, 0);
+        home.add(light2, 1);
+
+        System.out.println(home.sumTotal()); // 140
+
+        home.add(light3, 2);
+
+        System.out.println(home.sumTotal()); // 180
+
+        Lightbulb light4 = new Lightbulb("Bedroom", false, 40);
+        home.add(light4, 3); // Throws ArrayIndexOutOfBounds exception
     }
+
 }
