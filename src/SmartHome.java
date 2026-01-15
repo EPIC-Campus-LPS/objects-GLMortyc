1d1b333b020b18015c274fc469003b1df2239044
Morgan Chan <99379848+GLMortyc@users.noreply.github.com>
2026-01-15 13:25:09 -0700

    Update SmartHome.java

--- src/SmartHome.java
+++ src/SmartHome.java
@@ -1,2 +1,14 @@
 public class SmartHome {
+    private int slots;
+    private int sumTotal;
+
+    public SmartHome(int l){
+         slots = l;
+    }
+    public void add(Lightbulb light, int slot){
+        sumTotal += light.getPowerUsage(); ;
+    }
+    public int sumTotal() {
+        return sumTotal;
+    }
 }
